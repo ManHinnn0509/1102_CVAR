0520df66ffc155cde4f100e450ab0dfbc698033d
ManHinnn0509 <cheungmanhin0509@gmail.com>
2022-04-25 22:12:19 +0800

    Finish calculating disparity (Not sure)

--- HW-2/README.txt
+++ HW-2/README.txt
@@ -3,8 +3,20 @@ Due: 04/24
 
 Ref
 
-Depth Maps in OpenCV Python - Stereo Vision with Code and Examples
+- Depth Maps in OpenCV Python - Stereo Vision with Code and Examples
 https://youtu.be/jhOTm3MZDaY
+
+- Disparity of stereo images with Python and OpenCV
+https://rdmilligan.wordpress.com/2016/05/23/disparity-of-stereo-images-with-python-and-opencv/
+
+- Calculate depth disparity map using opencv
+* For calculating the depth from disparity,
+  OpenCV has the function reprojectImageTo3d (https://docs.opencv.org/2.4/modules/calib3d/doc/camera_calibration_and_3d_reconstruction.html#reprojectimageto3d)
+
+https://stackoverflow.com/questions/58113312/calculate-depth-disparity-map-using-opencv
+
+* depth = (baseline * focal length) / disparity)
+https://stackoverflow.com/questions/61053294/what-is-difference-between-disparity-and-depth
 
 https://docs.opencv.org/4.x/dd/d53/tutorial_py_depthmap.html
 
--- HW-2/main.py
+++ HW-2/main.py
@@ -12,30 +12,61 @@ def main():
     
     imgs = [cv2.imread(f"{p}/{i}", FLAG) for i in os.listdir(p)]
     # print(imgs)
+
+    # Downscale
+    # imgs = [cv2.pyrDown(i) for i in imgs]
     
     # 01 <- L ?
     # 02 <- R ?
     img01 = imgs[0]
     img02 = imgs[1]
 
-    stereo = cv2.StereoBM_create(
-            numDisparities=0,
-            blockSize=21
-    )
+    """
+    cv2.StereoBM_create() the disparity is computed by comparing the sum of absolute differences (SAD) of each 'block' of pixels.
+    In semi-global block matching or cv2.StereoSGBM_create() forces similar disparity on neighbouring blocks.
+    
+    This creates a more complete disparity map but is more computationally expensive.
+    """
+    
+    winSize = 3
+    minDisp = 16
+    numDisp = 112 - minDisp
 
-    depth = stereo.compute(img01, img02)
+    stereo = cv2.StereoSGBM_create(
+        minDisparity = minDisp,
+        numDisparities = numDisp,
+        blockSize = 16,
+        P1 = 8 * 3 * winSize * winSize,
+        P2 = 32 * 3 * winSize * winSize,
+        disp12MaxDiff = 1,
+        uniquenessRatio = 10,
+        speckleWindowSize = 100,
+        speckleRange = 32
+    )
     
     """
-    cv2.imshow("img01", img01)
-    cv2.imshow("img02", img02)
-    cv2.imshow("Depth", depth)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    stereo = cv2.StereoBM_create(
+        numDisparities = 16,
+        blockSize = 21
+    )
     """
 
-    plt.imshow(depth, "gray")
+    disp = stereo.compute(img01, img02)
+    disp = disp.astype(np.float32) / 16.0
+    disp = (disp - minDisp) / numDisp
+    
+    cv2.imshow("img01", img01)
+    cv2.imshow("img02", img02)
+    cv2.imshow("disp", disp)
+
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    
+    """
+    plt.imshow(disp)
     plt.axis("off")
     plt.show()
+    """
 
     print("--- End of Program ---")
 
