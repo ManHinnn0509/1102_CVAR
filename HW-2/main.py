c35f9a9d4abc48b2befa7120eb7718bf73d9418e
ManHinnn0509 <cheungmanhin0509@gmail.com>
2022-04-26 12:42:39 +0800

    Finish HW-2

--- HW-2/main.py
+++ HW-2/main.py
@@ -7,29 +7,90 @@ from matplotlib import pyplot as plt
 FLAG = cv2.IMREAD_GRAYSCALE
 
 def main():
-    p = "./databases/tsukuba"
-    # print(os.listdir(p))
-    
+
+    l = ["cones", "tsukuba", "venus"]
+    for name in l:
+        p = f"./databases/{name}"
+        # print(os.listdir(p)
+
+        mainCalc(p)
+
+    print("--- End of Program ---")
+
+def mainCalc(p):
+
+    # Dataset name
+    name = p.split("/")[-1]
+
     imgs = [cv2.imread(f"{p}/{i}", FLAG) for i in os.listdir(p)]
     # print(imgs)
 
     # Downscale
     # imgs = [cv2.pyrDown(i) for i in imgs]
     
-    # 01 <- L ?
-    # 02 <- R ?
-    img01 = imgs[0]
-    img02 = imgs[1]
+    imgL = imgs[0]
+    imgR = imgs[1]
 
     """
     cv2.StereoBM_create() the disparity is computed by comparing the sum of absolute differences (SAD) of each 'block' of pixels.
     In semi-global block matching or cv2.StereoSGBM_create() forces similar disparity on neighbouring blocks.
     
     This creates a more complete disparity map but is more computationally expensive.
+
+    calcDisp_2() <- StereoSGBM_create
+    calcDisp_1() <- StereoBM_create
+    """
+
+    disp = calcDisp_2(imgL, imgR)
+
+    """
+    depth = (baseline * focal length) / disp
+    
+    From: https://github.com/pablospe/tsukuba_db/blob/master/README.Tsukuba
+        baseline = 10
+        focal length = 615
+    """
+    depth = (10 * 615) / disp
+    print(depth)
+
+    # plt.imshow(disp)
+    # plt.imshow(depth)
+    # plt.axis("off")
+    # plt.show()
+
+    """
+    for img in [disp, depth]:
+        pltDisplay(img)
     """
     
-    winSize = 3
-    minDisp = 16
+    # OverflowError: cannot convert float infinity to integer??
+    saveMap(f"./{name}_depth.txt", depth)
+    # saveMap(f"./{name}_disp.txt", disp)
+
+    # Save img
+    # https://stackoverflow.com/questions/19239381/pyplot-imsave-saves-image-correctly-but-cv2-imwrite-saved-the-same-image-as
+    cv2.imwrite(f"./{name}_disp.png", disp * 255)
+
+    print(f"Done! ({p})")
+
+def saveMap(p, map):
+
+    # https://stackoverflow.com/questions/2844922/how-to-fix-this-python-error-overflowerror-cannot-convert-float-infinity-to-in
+    def fixINF(row):
+        INF = 1e6
+        return [max(min(x, INF), -INF) for x in row]
+
+    with open(p, "w+") as f:
+        for line in map:
+            line = fixINF(line)
+            np.savetxt(f, line, fmt="%d")
+
+def pltDisplay(img):
+    plt.figure()
+    plt.imshow(img)
+    plt.show()
+
+def calcDisp_2(imgL, imgR, winSize=3, minDisp=16):
     numDisp = 112 - minDisp
 
     stereo = cv2.StereoSGBM_create(
@@ -43,33 +104,28 @@ def main():
         speckleWindowSize = 100,
         speckleRange = 32
     )
-    
-    """
-    stereo = cv2.StereoBM_create(
-        numDisparities = 16,
-        blockSize = 21
-    )
-    """
 
-    disp = stereo.compute(img01, img02)
+    disp = stereo.compute(imgL, imgR)
     disp = disp.astype(np.float32) / 16.0
     disp = (disp - minDisp) / numDisp
-    
-    cv2.imshow("img01", img01)
-    cv2.imshow("img02", img02)
-    cv2.imshow("disp", disp)
+
+    return disp
+
+def calcDisp_1(imgL, imgR, numDisparities=16, blockSize=21):
+    stereo = cv2.StereoBM_create(
+        numDisparities=numDisparities,
+        blockSize=blockSize
+    )
+
+    disp = stereo.compute(imgL, imgR)
+    return disp
+
+def displayImages(l: list):
+    for img in l:
+        cv2.imshow(img)
 
     cv2.waitKey(0)
     cv2.destroyAllWindows()
-    
-    """
-    plt.imshow(disp)
-    plt.axis("off")
-    plt.show()
-    """
-
-    print("--- End of Program ---")
-
 
 if (__name__ == "__main__"):
     main()
